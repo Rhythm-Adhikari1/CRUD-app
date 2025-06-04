8d73a4c2a9f54b141333a261581d17a2d1c9d32c
Rhythm <ridam.adhikari61@gmail.com>
2025-06-04 20:28:47 +0545

    hi

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import  psycopg2
 from psycopg2.extras import RealDictCursor
-from time import time
+import time
 from .config import settings
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@\
 {settings.database_hostname}:{settings.database_port}/{settings.database_name}"
--- tests/conftest.py
+++ tests/conftest.py
@@ -14,7 +14,7 @@ from app.oauth2 import create_access_token
 
 # SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password123@localhost:5432/fastapi_test'
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@\
-{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
+{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
  
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
