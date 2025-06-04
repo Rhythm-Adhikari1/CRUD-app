3b923f51648ca80054404cae9f37ecbd349afee8
Rhythm <ridam.adhikari61@gmail.com>
2025-06-04 20:02:47 +0545

    final add

--- tests/database.py
+++ tests/database.py
@@ -13,7 +13,7 @@ from alembic import command # type: ignore
 
 # SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password123@localhost:5432/fastapi_test'
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@\
-{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
+{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
  
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
